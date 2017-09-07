c078bbc802a69d4962787f9a9e3c724bc5315ae8
David Gilbertson <gilbertson.david@gmail.com>
2017-09-07 15:29:39 +1000

    Fix unit tests

--- src/routes/components/Header/Header.test.js
+++ src/routes/components/Header/Header.test.js
@@ -1,35 +1,10 @@
-
-/* global it, describe, expect */
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {MemoryRouter} from 'react-router';
-import Header, {SidebarHeader} from './header';
+import Header from './Header';
 
-
-const Container = ({children}) => {
-  return (
-    <MemoryRouter initialEntries={['/']}>{children}</MemoryRouter>
-  )
-};
-
-describe('(Components) Header - header ', () => {
-  it('should match Snapshot', () => {
-    const renderedValue =  renderer.create(
-      <Container>
-        <Header openNav={() => {}}/>
-      </Container>
-    ).toJSON();
-    expect(renderedValue).toMatchSnapshot();
-  });
-});
-
-describe('(Components) Sidebar Header - header ', () => {
-  it('should match Snapshot', () => {
-    const renderedValue =  renderer.create(
-      <Container>
-        <SidebarHeader />
-      </Container>
-    ).toJSON();
-    expect(renderedValue).toMatchSnapshot();
-  });
+it('should match Snapshot', () => {
+  const renderedValue =  renderer.create(
+    <Header openNav={() => {}}/>
+  ).toJSON();
+  expect(renderedValue).toMatchSnapshot();
 });
