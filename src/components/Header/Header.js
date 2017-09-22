7c2d931140772f13b7e58e5d6d316a634b7b830c
Josh Mostafa <micapam@gmail.com>
2017-09-22 10:25:44 +1000

    Revert "WIP [skip ci]"
    This reverts commit da380e6db8818d888bebcb3e691afb9c91ee95f5.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,15 +5,7 @@ import CategoryNavContainer from '../CategoryNavContainer/CategoryNavContainer';
 import style from './Header.scss';
 
 const Header = props => (
-  <header className={style.header}>
-    <div className={style.header_govau_container}>
-      <span>*STAR*</span>
-      <strong>GOV.AU</strong>
-      <span>Official Australian Government Website</span>
-    </div>
-    <div className={style.header_project_status_container}>
-      <div className={style.header_project_status}>BETA</div>
-    </div>
+  <header className={style.wrapper}>
     <h1 className={style.title}>Header</h1>
 
     <CategoryNavContainer
