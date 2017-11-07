a4ef392ba74c856d6373e4d26981076a85b1f03e
Josh Mostafa <micapam@gmail.com>
2017-11-08 10:02:50 +1100

    Feat/sc 112 zero downtime (#52)
    * wip
    
    * actually install the plugin
    
    * plugin file perms

--- scripts/prepare_cf.sh
+++ scripts/prepare_cf.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
 # Build the app
 
 npm run build
@@ -9,6 +11,12 @@ npm run generate-city-data
 
 curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&version=6.17.0&source=github'
 sudo dpkg -i cf-cli_amd64.deb
+
+# Install Autopilot plugin
+
+curl -v -L -o ~/autopilot-linux https://github.com/contraband/autopilot/releases/download/0.0.3/autopilot-linux
+chmod a+x ~/autopilot-linux
+cf install-plugin -f ~/autopilot-linux
 
 # Log in to CF
 
