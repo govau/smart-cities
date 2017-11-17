dcd60e90ebecac7dbfd96ad1c153900ddc0666a3
David Gilbertson <gilbertson.david@gmail.com>
2017-11-17 11:32:17 +1100

    Fix colors the wrong way around (#73)

--- src/helpers/getColorRange.js
+++ src/helpers/getColorRange.js
@@ -3,9 +3,9 @@ import { COLOR_NAMES } from '../constants';
 
 // This is only ever used by charts, hence the lack of a CONTEXT set of colors.
 const CATEGORY_COLOR_MAP = {
-  [COLOR_NAMES.JOBS]: [200, 500, 800, 950],
+  [COLOR_NAMES.JOBS]: [200, 400, 800, 950],
   [COLOR_NAMES.HOUSING]: [200, 400, 700, 950],
-  [COLOR_NAMES.INFRASTRUCTURE]: [200, 400, 800, 950],
+  [COLOR_NAMES.INFRASTRUCTURE]: [200, 500, 800, 950],
   [COLOR_NAMES.LIVEABILITY]: [200, 400, 700, 950],
   [COLOR_NAMES.INNOVATION]: [200, 400, 800, 950],
   [COLOR_NAMES.PLANNING]: [200, 400, 800, 950],
