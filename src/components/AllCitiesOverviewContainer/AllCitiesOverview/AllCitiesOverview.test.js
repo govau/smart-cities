453d52d137ad7afdc7c94f08481609685c7be249
brett-sargeant <brett.sargeant@infrastructure.gov.au>
2019-03-06 11:18:21 +1100

    reseting overview file so that GA-ID can be published.

---
+++ src/components/AllCitiesOverviewContainer/AllCitiesOverview/AllCitiesOverview.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AllCitiesOverview from './AllCitiesOverview';
+
+jest.mock('../../CategoryOverviews/CategoryOverviews', () => 'CategoryOverviews');
+jest.mock('../../Icon/Icon', () => 'Icon');
+
+const defaultProps = {
+  cities: [
+    {
+      id: 'perth',
+      indicators: {},
+    },
+    {
+      id: 'sydney',
+      indicators: {},
+    },
+  ],
+};
+
+it('should match Snapshot', () => {
+  const component = shallow(
+    <AllCitiesOverview {...defaultProps} />
+  );
+
+  expect(component.debug()).toMatchSnapshot();
+});
